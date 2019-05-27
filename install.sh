7e19f862b0bc0245e735861bec7402f55fd6fabd
Murilo Santana <mvrilo@gmail.com>
2019-05-27 08:48:28 -0300

    mac/linux separation

--- install.sh
+++ install.sh
@@ -2,16 +2,53 @@
 
 set -e
 
-main() {
+install_mac() {
+  if ! which brew 2>/dev/null; then
+    /usr/bin/ruby -e \
+      "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 2>/dev/null >/dev/null
+  fi
+
 	brew tap 'homebrew/bundle'
 	brew bundle install --verbose
 
-	git submodule update --init
-
 	pip  install -U pip neovim
 	pip3 install -U pip neovim
+	gem install -u neovim
 
-	sudo gem install -u bundler neovim
+	nvim +PlugUpgrade +qa
+	nvim +PlugUpdate +qa
+	nvim +UpdateRemotePlugins +qa
+}
+
+install_linux() {
+	sudo apt update
+	sudo apt install -y \
+		bash \
+		git \
+		golang \
+		htop \
+		jq \
+		nmap \
+		nodejs \
+		npm \
+		python \
+		python-pip \
+		python3 \
+		python3-pip \
+		ruby \
+		tmux \
+		tree \
+		vim
+	}
+
+main() {
+  if [ "$(uname -s)" = "Darwin" ]; then
+		install_mac
+	else
+		install_linux
+	fi
+
+	git submodule update --init
 
 	npm i -g \
 		yarn \
@@ -22,11 +59,7 @@ main() {
 		neovim \
 		serve
 
-	nvim \
-		+PlugUpgrade \
-		+PlugInstall \
-		+UpdateRemotePlugins \
-		+qa
+  go get -v -u github.com/mvrilo/go-cpf/cmd/cpf
 }
 
 main
